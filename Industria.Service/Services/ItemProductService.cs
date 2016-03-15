09a3e4e0cf378aa2b35af8c4f6cd1c9c23d07e0d
ridwanf <ridwanfansuri@gmail.com>
2016-03-15 13:25:37 +0700

    add service and presentation layer

---
+++ Industria.Service/Services/ItemProductService.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Core;
+using Industrial.Data.Domain;
+using Industrial.Repository.Repositories;
+using Industrial.Service.ViewModel.Master;
+
+namespace Industrial.Service.Services
+{
+    public class ItemProductService
+    {
+        private readonly IItemProductRepository _itemRepository;
+        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+        public ItemProductService(IItemProductRepository itemRepository, IUnitOfWorkFactory unitOfWorkFactory)
+        {
+            _itemRepository = itemRepository;
+            _unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+
+        public IEnumerable<ItemProductViewModel> GetAll()
+        {
+            var viewData = new List<ItemProductViewModel>();
+            List<ItemProduct> data = _itemRepository.FindAll(a => a.IsActive).ToList();
+            Mapper.Map(data, viewData);
+            return viewData;
+        }
+
+        public ItemProductViewModel FindById(int id)
+        {
+            var data = new ItemProductViewModel();
+            ItemProduct result = _itemRepository.FindBy(a => a.Id == id).First();
+            if (result != null)
+            {
+                Mapper.Map(result, data);
+                return data;
+            }
+            return null;
+        }
+
+        public ItemProductViewModel Create(ItemProductViewModel item)
+        {
+            var data = new ItemProduct();
+            Mapper.Map(item, data);
+            using (_unitOfWorkFactory.Create())
+            {
+                _itemRepository.Add(data);
+            }
+
+            Mapper.Map(data, item);
+            return item;
+        }
+
+        public ItemProductViewModel Edit(ItemProductViewModel item)
+        {
+            var data = new ItemProduct();
+            Mapper.Map(item, data);
+            using (_unitOfWorkFactory.Create())
+            {
+                _itemRepository.Update(data);
+            }
+            Mapper.Map(data, item);
+            return item;
+        }
+
+        public ItemProductViewModel Delete(int id)
+        {
+            var item = _itemRepository.FindBy(a => a.Id == id).FirstOrDefault();
+            var itemView = new ItemProductViewModel();
+            if (item == null)
+                return null;
+            else
+            {
+                _itemRepository.SoftDelete(item);
+                Mapper.Map(item, itemView);
+                return itemView;
+            }
+        }
+    }
+}
---
+++ Industria.Service/ViewModel/Master/ItemProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial.Service.ViewModel.Master
+{
+    public class ItemProductViewModel : IViewModelBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
